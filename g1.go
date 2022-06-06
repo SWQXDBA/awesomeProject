134243de748737fee5860935c57dfc37398bd91d
SWQXDBA <983110853@qq.com>
2022-06-06 15:09:51 +0800

    1

--- g1.go
+++ g1.go
@@ -2,7 +2,16 @@ package main
 
 import "fmt"
 
+type Student struct {
+}
+
 func main() {
 
-	 fmt.Printf("你好世界%d",1)
+	numb := 10
+	fmt.Printf("你好世界%d", numb)
+
+	for i := 0; i < 200; i++ {
+		fmt.Printf("你好世界%d", i)
+	}
+
 }
